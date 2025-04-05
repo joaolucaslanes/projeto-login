bc53450240e88a3367b74eae8104c62e585eeba5
João Lucas Lanes <lanesjoaolucas@gmail.com>
2025-04-05 15:20:29 -0300

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const botaoEsqueci = document.getElementById("btn-esqueci");
+
+  botaoEsqueci.addEventListener("click", function () {
+    alert("Redefinição de senha: Verifique seu e-mail para recuperar sua senha.");
+  });
+});
